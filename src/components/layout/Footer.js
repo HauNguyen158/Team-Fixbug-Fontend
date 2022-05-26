8c886be9fe06c772456890bf83443d17ec91b3dd
Hau Nguyen <haunguyen15800@gmail.com>
2022-05-26 18:53:07 +0700

    test

--- src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import { Layout } from 'antd';
-
-export default () => (
-    <>
-        <div className='py-3 bg-primary text-center text-white'>
-            <span>© 2021, made with by NDT</span>
-        </div>
-    </>
-);
--- src/components/layout/Main.js
+++
@@ -1,8 +0,0 @@
-
-export default () => (
-    <>
-        <div className="bg-white ms-3 h-100 rounded-3">
-            <span>Content</span>
-        </div>
-    </>
-);
